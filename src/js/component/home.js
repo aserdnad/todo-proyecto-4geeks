604c58dccddeb6ef2fab75d008dcfd81ff95bc80
aserdnad <andreasgamardo@gmail.com>
2021-04-18 20:54:49 +0200

    todo list proyecto

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,68 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { Jumbotron, Form, ListGroup } from "react-bootstrap";
+import { useState } from "react";
 
 //create your first component
 export function Home() {
+	const [tareas, setTareas] = useState([]);
+	const [agregar, setAgregar] = useState("");
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		let copia = tareas;
+		if (agregar !== "") {
+			setTareas([...copia, agregar]);
+			setAgregar("");
+		}
+	};
+
+	const eliminar = index => {
+		// let copia = [...tareas];
+		// copia.splice(index, 1);
+		// setTareas(copia);
+		setTareas(tareas.filter((tarea, i) => i !== index));
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="contenido">
+			<h1 className="mt-5">todos</h1>
+			<Jumbotron className="container bg-white">
+				<Form onSubmit={e => handleSubmit(e)}>
+					<Form.Group controlId="formTodo">
+						<Form.Control
+							type="text"
+							placeholder="Agrega tu tarea manito"
+							value={agregar}
+							onChange={e => setAgregar(e.target.value)}
+						/>
+					</Form.Group>
+				</Form>
+				<ListGroup>
+					{tareas.length === 0 ? (
+						<ListGroup.Item>
+							No hay tareas pendientes, agregue tarea
+						</ListGroup.Item>
+					) : (
+						tareas.map((tarea, index) => {
+							return (
+								<ListGroup.Item
+									key={index}
+									className="escondido d-flex justify-content-between">
+									{tarea}{" "}
+									<span onClick={e => eliminar(index)}>
+										❌
+									</span>
+								</ListGroup.Item>
+							);
+						})
+					)}
+					{tareas.length > 0 && (
+						<ListGroup.Item className="final">
+							Quedan {tareas.length} pendientes
+						</ListGroup.Item>
+					)}
+				</ListGroup>
+			</Jumbotron>
 		</div>
 	);
 }
